a2b4c4243970b5c8af378de1fc6d8f95b5332e3e
Can Bölük <can.boluk89@gmail.com>
2021-03-25 01:12:53 +0100

    Fixing double-s in list-view.

--- analysis/public/listview.js
+++ analysis/public/listview.js
@@ -9,7 +9,7 @@ const header = [
     { key: "extension",        title: "Extension",         detail: "Extension set according to XED" },
     { key: "cpl",              title: "CPL",               detail: "Privilege level this instruction runs at according to XED" },
     { key: "outOfOrder",       title: "Div cycles",        detail: "Cycles spent during out-of-order execution of the division." },
-    { key: "mits",             title: "µOps (MITS)",       detail: "# of µOps decoded by the micro-instruction translation engine" },
+    { key: "mits",             title: "µOps (MITE)",       detail: "# of µOps decoded by the micro-instruction translation engine" },
     { key: "ms",               title: "µOps (MS)",         detail: "# of µOps decoded by the microcode sequencer" },
     { key: "serializing",      title: "Serializing",       detail: "Enforces ordering of the pipeline, if not also a speculative fence, means only applicable to the memory accesses" },
     { key: "speculationFence", title: "Speculation Fence", detail: "Halts speculative execution" },
